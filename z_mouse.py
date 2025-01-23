b1c1490d8f1ec5ff352f04f0b843cb646c5dfc0d
burningke <12174zy20612@gmail.com>
2025-01-23 08:50:56 +0100

    Create z_mouse.py

---
+++ z_mouse.py
@@ -0,0 +1,12 @@
+import pydirectinput
+import time
+
+while True:
+    pydirectinput.click()
+    time.sleep(0.1)
+    pydirectinput.press('y')
+    time.sleep(0.1)  
+    pydirectinput.press('enter')
+    time.sleep(0.1)  
+    pydirectinput.press('y')
+    time.sleep(0.1)  
