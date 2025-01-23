94e7d8e50a4bc03cd3e7dbfaf7c3fed2a833c9f0
burningke <12174zy20612@gmail.com>
2025-01-23 08:57:02 +0100

    Update z_mouse.py

--- z_mouse.py
+++ z_mouse.py
@@ -1,12 +1,13 @@
 import pydirectinput
 import time
 
+time.sleep(5)
 while True:
     pydirectinput.click()
-    time.sleep(0.1)
+    time.sleep(0.5)
     pydirectinput.press('y')
-    time.sleep(0.1)  
+    time.sleep(0.5)  
     pydirectinput.press('enter')
-    time.sleep(0.1)  
+    time.sleep(0.5)  
     pydirectinput.press('y')
-    time.sleep(0.1)  
+    time.sleep(0.5)  
